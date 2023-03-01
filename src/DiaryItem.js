aa3bef0a8ebc455bb32440b3334e7a62484237a2
altmit <chlwodnjs34@gmail.com>
2023-03-02 00:46:38 +0900

    [feat] 감정 일기장 일기 추가, 삭제 까지 구현 되었습니다.

---
+++ src/DiaryItem.js
@@ -0,0 +1,26 @@
+const DiaryItem = ({ id, author, content, emotion, create_date, onDelete }) => {
+  return (
+    <div className="DiaryItem">
+      <div className="info">
+        <span>
+          작성자 : {author} | 감정점수 : {emotion}
+        </span>
+        <br />
+        <span className="date">{new Date(create_date).toLocaleString()}</span>
+        <div className="content">{content}</div>
+        <button
+          onClick={() => {
+            console.log(id);
+            if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+              onDelete(id);
+            }
+          }}
+        >
+          삭제하기
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryItem;
