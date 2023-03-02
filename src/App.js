fab6b6f5739323656258e1f339e65635537761c9
altmit <chlwodnjs34@gmail.com>
2023-03-03 02:24:32 +0900

    [feat] jsonplaceholder의 api를 이용하여 데이터 가공후 일기 데이터로 활용하였습니다

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import "./App.css";
 
-function App() {
+// https://jsonplaceholder.typicode.com/comments
+const App = () => {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((e) => {
+      return {
+        author: e.email,
+        content: e.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        create_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const create_date = new Date().getTime();
@@ -37,6 +60,6 @@ function App() {
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
-}
+};
 
 export default App;
